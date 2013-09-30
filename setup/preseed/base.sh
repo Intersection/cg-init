6db1377bcd5a123d33cd5118f23f33fa4707be25
Vikash Dat <vikash.dat@controlgroup.com>
2013-09-30 17:07:29 -0400

    added preseed directory

---
+++ setup/preseed/base.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo 'ubuntu ALL=NOPASSWD:ALL' > ubuntu
+echo 'ubuntu' | sudo -S chmod 440 ubuntu
+echo 'ubuntu' | sudo -S chown root:root ubuntu
+echo 'ubuntu' | sudo -S mv ubuntu /etc/sudoers.d/
+
+sleep 30;
+sudo apt-get -y update;
+sudo apt-get install -y git puppet zip virtualbox-guest-additions-iso;
+
