1ab6033c771695c33455c5323d58e9d6f4cb9a46
Vikash Dat <vikash.dat@controlgroup.com>
2013-10-01 16:18:10 -0400

    adding ssh user

--- setup/install.sh
+++ setup/install.sh
@@ -1,4 +1,24 @@
-#do packer install
-#vagrant init
-#vagrant up
-#vagrant ssh
+
+if [ -z "$1" ] 
+then
+  echo "Usage: install arch"
+else
+  #do packer install
+  echo "Starting packer build..."
+  packer build $1/packer-$1.json
+  echo "Packer build complete..."
+  echo "Starting Vagrant initilization..."
+  #vagrant init
+  vagrant init dev packer_virtualbox_virtualbox.box 
+  echo "Vagrant initilization complete..."
+  #add ssh user
+  echo "Adding ssh user..."
+  sed -i "" '36i\'$'\n''config.ssh.username = "ubuntu"' Vagrantfile 
+  echo "Bringing Box Up..."
+  #vagrant up
+  vagrant up
+  echo "Vagrant box is up..."
+  echo "Attempting to ssh to box..."
+  #vagrant ssh
+  vagrant ssh
+fi
--- setup/preseed/base.sh
+++ setup/preseed/base.sh
@@ -8,6 +8,8 @@ echo 'ubuntu' | sudo -S mv ubuntu /etc/sudoers.d/
 sleep 30;
 sudo apt-get -y update;
 sudo apt-get install -y git puppet zip virtualbox-guest-additions-iso;
+mkdir /home/ubuntu/.ssh;
+wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O /home/ubuntu/.ssh/authorized_keys;
 
 #Language specific configurations can be included below
 #source ./java-preseed/base.sh
