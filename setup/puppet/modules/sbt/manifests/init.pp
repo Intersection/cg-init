99661fa73cee382c3ec74f42528d5df218b1af36
Vikash Dat <vikash.dat@controlgroup.com>
2013-10-07 15:41:13 -0400

    add puppet module for sbt

---
+++ setup/puppet/modules/sbt/manifests/init.pp
@@ -0,0 +1,32 @@
+class sbt {
+
+ file { '/tmp/sbt-launch.jar':
+    ensure => present,
+    require => Exec['Fetch sbt'],
+  }
+
+exec { 'Fetch sbt':
+    cwd => '/tmp',
+    command => 'wget http://repo.typesafe.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/0.13.0/sbt-launch.jar',
+    creates => '/tmp/sbt-launch.jar',
+  }
+
+
+file { "/usr/bin/sbt-launch.jar":
+    owner => ubuntu,
+    group => root,
+    source => "/tmp/sbt-launch.jar",
+    require => Exec['Fetch sbt'];
+}
+
+file {'sbt':
+      path    => '/usr/bin/sbt',
+      ensure  => present,
+      mode    => 0775,
+      content => "java -Xms512M -Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256M -jar /usr/bin/sbt-launch.jar \"$@\"",
+      require => File['/usr/bin/sbt-launch.jar'];  
+  }
+
+
+}
+
