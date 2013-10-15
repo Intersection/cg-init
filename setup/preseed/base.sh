cc4ce6753912941ddeb4f59245643c46d5bae962
Vikash Dat <vikash.dat@controlgroup.com>
2013-10-15 10:23:00 -0400

    added logic to distingish between whats needed for an ami and vbox

--- setup/preseed/base.sh
+++ setup/preseed/base.sh
@@ -7,9 +7,14 @@ echo 'ubuntu' | sudo -S mv ubuntu /etc/sudoers.d/
 
 sleep 30;
 sudo apt-get -y update;
-sudo apt-get install -y git puppet zip virtualbox-guest-additions-iso;
-mkdir /home/ubuntu/.ssh;
-wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O /home/ubuntu/.ssh/authorized_keys;
+
+#we only want to include virtualbox and ssh keys if this is vagrant build, not AMI
+hostnameStr=$(hostname)
+if [[ ${hostnameStr} =~ ubuntu-* ]]; then
+    sudo apt-get install -y git puppet zip virtualbox-guest-additions-iso;
+    mkdir /home/ubuntu/.ssh;
+    wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O /home/ubuntu/.ssh/authorized_keys;
+fi
 
 #Utilize Puppet to load packages
 sudo puppet apply --debug --verbose --modulepath=/home/ubuntu/puppet/modules /home/ubuntu/puppet/manifests/site.pp;
